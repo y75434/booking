7b2a8c3fa00bfb0b1659455ac59393fa03d5f97a
y75434 <she85213@gmail.com>
2020-12-11 22:41:24 +0800

    Update 2020-12-11T14:40:51.368Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,16 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 //# sourceMappingURL=all.js.map
