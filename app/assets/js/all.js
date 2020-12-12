c8bf8eaca20fd29402f1eb29a077a03339156f69
y75434 <she85213@gmail.com>
2020-12-12 23:51:54 +0800

    1212

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,13 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
